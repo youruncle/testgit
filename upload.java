13d232f090127e246651f1172b796fc6ba610eb4
youruncle <793732746@qq.com>
2016-07-02 21:09:34 +0800

    Create upload.java

---
+++ upload.java
@@ -0,0 +1,19 @@
+package strtest;
+
+import java.util.Scanner;
+
+public class test {
+	
+	
+	
+	public static void main(String[] args)
+	{
+		String[] i = {"1","2","3"};
+		String[] j = new String[5];
+		j[1] = "4";
+		System.out.println(i[1]);
+		System.out.println(j[1]);
+	}
+	
+
+}
